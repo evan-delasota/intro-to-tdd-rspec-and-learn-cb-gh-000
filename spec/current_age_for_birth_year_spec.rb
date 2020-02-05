b89e885b3eabe0bdaec9f1d816e7347d47b9f1b9
Evan Delasota <evan.delasota@gmail.com>
2020-02-05 02:09:29 +0000

    Automatically backed up by Learn

--- spec/current_age_for_birth_year_spec.rb
+++ spec/current_age_for_birth_year_spec.rb
@@ -1,5 +1,4 @@
 require_relative '../current_age_for_birth_year.rb'
-require "spec_helper"
 
 describe "current_age_for_birth_year method" do
   it "returns the age of a person based on the year of birth" do
